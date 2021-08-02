867be3b443de3ec249f65c6876fba218cc98294b
Subhas Bhat <bhatsubhas@gmail.com>
2021-08-02 17:21:59 +0530

    Add feature to save and read deck from file

--- main.go
+++ main.go
@@ -5,6 +5,16 @@ import "fmt"
 func main() {
 	cards := newDeck()
 
+	fmt.Println("Converting to a string")
+	fmt.Println(cards.toString())
+
+	fmt.Println("Saving to a file")
+	cards.saveToFile("mycards.txt")
+
+	fmt.Println("Reading from a file")
+	mycards := newDeckFromFile("mycards.txt")
+	mycards.print()
+
 	hand, remainingCards := deal(cards, 3)
 	fmt.Println("Cards in hand are:")
 	hand.print()
