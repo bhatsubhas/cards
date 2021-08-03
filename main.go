f3450f6843bfc8dee5ba6e22e1928daf500c6e1f
Subhas Bhat <bhatsubhas@gmail.com>
2021-08-03 15:12:16 +0530

    Update main function to test deck shuffle function

--- main.go
+++ main.go
@@ -5,20 +5,9 @@ import "fmt"
 func main() {
 	cards := newDeck()
 
-	fmt.Println("Converting to a string")
-	fmt.Println(cards.toString())
-
-	fmt.Println("Saving to a file")
-	cards.saveToFile("mycards.txt")
-
-	fmt.Println("Reading from a file")
-	mycards := newDeckFromFile("mycards.txt")
-	mycards.print()
-
-	hand, remainingCards := deal(cards, 3)
-	fmt.Println("Cards in hand are:")
-	hand.print()
-
-	fmt.Println("Remaining cards in the deck are:")
-	remainingCards.print()
+	fmt.Println("Before shuffling")
+	cards.print()
+	fmt.Println("After shuffling")
+	cards.shuffle()
+	cards.print()
 }
